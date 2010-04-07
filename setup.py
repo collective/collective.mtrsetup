27ca22fd0dbb94b60a0c7a7cc43fa08043de2746
Jonas Baumann <j.baumann@4teamwork.ch>
2010-04-07 12:18:43 +0000

    created package with paster

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+
+setup(name='collective.mtrsetup',
+      version=version,
+      description="Extension for GenericSetup, adding support for import / export of mimetypes_registry",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='generic setup gs mimetypes registry import export',
+      author='Jonas Baumann, 4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='http://svn.plone.org/svn/collective/collective.mtrsetup',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )

5073ce37c9b61351876a2d62382de737715d8ee0
Jonas Baumann <j.baumann@4teamwork.ch>
2010-04-07 12:52:14 +0000

    added buildout.cfg for tests

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@ import os
 
 version = '1.0a1'
 
+tests_require = ['zope.testing',]
+
 setup(name='collective.mtrsetup',
       version=version,
       description="Extension for GenericSetup, adding support for import / export of mimetypes_registry",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Programming Language :: Python",
@@ -22,10 +24,17 @@ setup(name='collective.mtrsetup',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'Products.GenericSetup',
+        'z3c.autoinclude',
+        # -*- Extra requirements: -*-
+        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+#      test_suite = 'collective.mtrsetup.tests.test'
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
